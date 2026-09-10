545c9abf8d73cf7f417f1b2af3a80e19b730d67e
carolinelam-eng <caroline.lammers@web.de>
2026-09-10 18:51:05 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,2 @@
+self.addEventListener('install',e=>e.waitUntil(caches.open('ameloua-v3').then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./app-core.js']))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
