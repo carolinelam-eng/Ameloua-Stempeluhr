055e9451365a0c0011218f334fcac5985bd69e16
carolinelam-eng <caroline.lammers@web.de>
2026-09-10 22:38:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ameloua-v5-3';const FILES=['./','./index.html','./app-core.js?v=5.3','./manifest.webmanifest'];
+const CACHE='ameloua-v5-4';const FILES=['./','./index.html','./app-core.js?v=5.4','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
